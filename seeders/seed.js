98b58d188081dec14ea6ab7f200ca5e36d834194
Qian Dong <qdong327@gmail.com>
2021-01-02 14:05:09 -0500

    two projects down

--- seeders/seed.js
+++ seeders/seed.js
@@ -14,17 +14,45 @@ let projectSeed = [
             {
                 type: "wood",
                 name: "plywood sheets",
-                size: "4ft x 8ft x 3/4in"
+                size: "4ft x 8ft x 11/16in"
             }
         ],
         tools: ["level", "hammer", "sandpaper", "drill", "1in screws"],
-        optional: ["paint", "shelf pins (removeable shelves)", "5mm shelf pin drill bit"]
+        optional: ["paint or wood stain", "shelf pins (removeable shelves)", "5mm shelf pin drill bit"],
         inputWidth: "",
         inputDepth: "",
         inputHeight: "",
 		imgMain: "/public/construction-icons/bookcase-main.png",
-		imgEx: "/public/construction-icons/bookcase-ex.png",
-	},
+		imgEx: "/public/construction-icons/bookcase-ex.png"
+    },
+    {
+        project: "chair",
+        description: "A simple chair with an open back and a footrail.",
+        materials: [
+            {
+                type: "wood",
+                name: "2x4 nominal studs",
+                size: "8ft lengths of 2x4 (1.5in x 3.5in)"
+            },
+            {
+                type: "wood",
+                name: "1-inch rod",
+                size: "4ft length"
+            },
+            {
+                type: "wood",
+                name: "plywood sheets",
+                size: "4ft x 8ft x 11/16in"
+            }
+        ],
+        tools: ["level", "hammer", "sandpaper", "drill", "2in screws", "1-inch diameter drill bit"],
+        optional: ["paint or wood stain"],
+        inputWidth: "",
+        inputDepth: "",
+        inputHeight: "",
+		imgMain: "/public/construction-icons/chair-main.png",
+		imgEx: "/public/construction-icons/chair-ex.png"
+    },
 ];
 
 db.Project.deleteMany({})
