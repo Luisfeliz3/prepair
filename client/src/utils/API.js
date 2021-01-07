e84744e137b3f6fb9654c8b735bd086f10eed395
Qian Dong <qdong327@gmail.com>
2021-01-07 00:28:51 -0500

    added test API function in API.js below sample comment stuff

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -18,3 +18,24 @@ export default {
     return axios.post("/api/comments", commentData);
   }
 };
+
+// Note: This API costs money! Test sparingly:
+  // Requests: $0.005 / use
+  // Results: $1e-8 / use
+
+// Test API function
+const options = {
+  method: 'GET',
+  url: 'https://sku-io.p.rapidapi.com/request',
+  params: {param: 'drill', store: 'HOMEDEPOT', function: 'getSearch'},
+  headers: {
+    'x-rapidapi-key': '6a6c93e695msh75b8b667572cb64p157cf7jsnea4fbc5808b0',
+    'x-rapidapi-host': 'sku-io.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
