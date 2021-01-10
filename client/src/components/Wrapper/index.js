57e636d3ce8532c2a3d0b5f21e270e43ba7728a8
Qian Dong <qdong327@gmail.com>
2021-01-10 09:02:59 -0500

    clarified routing

--- client/src/components/Wrapper/index.js
+++ client/src/components/Wrapper/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-import "./style.css"
+import placeholderclose from "../../static/wood.png";
+import "./style.css";
 
 export function Wrapper() {
-    return <div className="wrap">
-        I'm a wrapper!
-    </div>
+	return (
+		<div className="wrap">
+			<img
+				src={placeholderclose}
+				className="background-closeup"
+				alt="woodplanks"
+			/>
+			<h1 className="subtitle">Create a New Project</h1>
+		</div>
+	);
 }
 
 export default Wrapper;
--- client/src/pages/SavedProjects.js
+++ client/src/pages/SavedProjects.js
@@ -1,8 +1,13 @@
 import { Grid, Segment } from "semantic-ui-react";
+import Wrapper from "../components/Wrapper"
 import React from "react";
 
 function SavedProjects() {
-	return <h1>saved Projects</h1>;
+	return (
+		<Wrapper>
+			<h1>saved Projects</h1>
+		</Wrapper>
+	);
 }
 
 export default SavedProjects;
