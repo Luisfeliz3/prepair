b96f36f347c1be64de914d4a04391cabd61c30b3
Luis Feliz <Luisfeliz@gmail.com>
2021-01-13 20:08:54 -0500

    chore(packages):login

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,37 @@
 // import React, { useEffect, useState } from "react";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Container from "./components/Container";
+import userAPI from "./utils/userAPI";
 import Nav from "./components/Nav";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import NewProject from "./pages/NewProject";
 import SavedProjects from "./pages/SavedProjects";
+import ProtectedRoute from "./components/ProtectedRoute"
+import NoMatch from "./pages/NoMatch";
 import "./App.css";
 
 function App() {
-	// const [userState, setUserState] = useState({});
+	const [userState, setUserState] = useState({});
 	document.title = "Prepair";
+
+
+	useEffect(() => { 
+		// auth user on first render
+	   authenticate() 
+	}, []);
+
+		//user authentication
+		function authenticate() {
+			return userAPI.authenticateUser()
+				.then(({ data }) => {
+					console.log('user:', data );
+				setUserState(data);
+				})
+				.catch((err) => console.log('registered user:', err.response));
+		}
+
 	return (
 		<BrowserRouter basename={process.env.PUBLIC_URL || "/prepair"}>
 			<div className="App">
@@ -21,17 +41,26 @@ function App() {
 							<Container />
 						</Route>
 						<Route exact path="/login" className="App-link" component={Login}>
-							<Login />
+							<Login 
+							// {...props}
+							userState={userState}
+							setUserState={setUserState}
+							/>
 						</Route>
 						<Route exact path="/signup" className="App-link" component={Signup}>
-							<Signup />
+							<Signup 
+								// {...props}
+								authenticate={authenticate}
+								user={userState}
+							/>
 						</Route>
-						<Route exact path="/newproject" className="App-link" component={NewProject}>
+						<ProtectedRoute exact path="/newproject" className="App-link" component={NewProject}>
 							<NewProject />
-						</Route>
-						<Route exact path="/myprojects" className="App-link" component={SavedProjects}>
+						</ProtectedRoute>
+						<ProtectedRoute exact path="/myprojects" className="App-link" component={SavedProjects}>
 							<SavedProjects />
-						</Route>
+						</ProtectedRoute>
+					<Route component={NoMatch} />
 					</Switch>
 			</div>
 		</BrowserRouter>
