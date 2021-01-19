2b9c784b7a97a8c6d061d8c025ef7053022b1a9b
Qian Dong <qdong327@gmail.com>
2021-01-19 00:56:11 -0500

    made changes to the modal

--- client/src/components/Popup/index.js
+++ client/src/components/Popup/index.js
@@ -5,15 +5,20 @@ import {
 	Dropdown,
 	InputGroup,
 	FormControl,
+	Form,
 	DropdownButton,
 	Row,
 	Col,
 } from "react-bootstrap";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import wood from "../../static/wood.png";
-import expl from "../../static/construction-icons/table-ex.svg";
+import twentyfive from "../../static/twentyfive.png";
+import fifty from "../../static/fifty.png";
+import seventyfive from "../../static/seventyfive.png";
+import hundred from "../../static/hundred.png";
+import expl from "../../static/construction-icons/chair-ex.svg";
 import "./styles.css";
+
 const Popup = (props) => {
 	console.log(props);
 	const [show, setShow] = useState(true);
@@ -33,135 +38,101 @@ const Popup = (props) => {
 				backdrop="static"
 				keyboard={false}
 				size="lg"
+				dialogClassName="modal-90w"
 				aria-labelledby="contained-modal-title-vcenter"
 				centered
 			>
 				<Modal.Header closeButton>
-					<Modal.Title>Project Name</Modal.Title>
+					<Modal.Title>
+						A simple {props.projects.data[0].projectName}
+					</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>
+				<Modal.Body className="short">
 					<Row>
-						<Col xs={6} md={10} className="row justify-content-around">
-							<CircularProgressbar value={percentage} text={`${percentage}%`} />
-							<img
-								src={expl}
-								className="explodedDiagram"
-								alt="exploded-diagram"
-							/>
-						</Col>
-
-						<section>
-							<form className="col-4">
-								<div className="form-group"></div>
-								<div>
-									Width (in):
-									<DropdownButton
-										className="dropdown-basic-button"
-										title="Select"
-									>
-										<Dropdown.Item href="#/action-1">12</Dropdown.Item>
-										<Dropdown.Item href="#/action-2">
-											24
-										</Dropdown.Item>
-										<Dropdown.Item href="#/action-3">
-											36
-										</Dropdown.Item>
-									</DropdownButton>
-								</div>
-
-								<div>
-									Height (in):
-									<DropdownButton
-										className="dropdown-basic-button"
-										title="Select"
-									>
-										<Dropdown.Item href="#/action-1">18</Dropdown.Item>
-										<Dropdown.Item href="#/action-2">
-											24
-										</Dropdown.Item>
-										<Dropdown.Item href="#/action-3">
-											30
-										</Dropdown.Item>
-									</DropdownButton>
-								</div>
-								<div>
-									Depth (in):
-									<DropdownButton
-										className="dropdown-basic-button"
-										title="Select"
-									>
-										<Dropdown.Item href="#/action-1">24</Dropdown.Item>
-										<Dropdown.Item href="#/action-2">
-											30
-										</Dropdown.Item>
-										<Dropdown.Item href="#/action-3">
-											36
-										</Dropdown.Item>
-									</DropdownButton>
-								</div>
+					<p>Description: {props.projects.data[0].description}</p>
+					<Col xs={6} md={12} className="row justify-content-around">
+						{/* <CircularProgressbar value={percentage} text={`${percentage}%`} /> */}
+					</Col>
+					{/* <section> */}
+					<Form className="col-4">
+						<div className="form-group">
+							<div>
+								<Form.Group controlId="exampleForm.ControlSelect1">
+									<Form.Label>Width (in):</Form.Label>
+									<Form.Control as="select">
+										<option>12</option>
+										<option>24</option>
+										<option>36</option>
+									</Form.Control>
+								</Form.Group>
+							</div>
+							<div>
+								<Form.Group controlId="exampleForm.ControlSelect1">
+									<Form.Label>Height (in):</Form.Label>
+									<Form.Control as="select">
+										<option>18</option>
+										<option>24</option>
+										<option>30</option>
+									</Form.Control>
+								</Form.Group>
+							</div>
+							<div>
+								<Form.Group controlId="exampleForm.ControlSelect1">
+									<Form.Label>Depth (in):</Form.Label>
+									<Form.Control as="select">
+										<option>24</option>
+										<option>30</option>
+										<option>36</option>
+									</Form.Control>
+								</Form.Group>
+							</div>
 							<Button id="calculate-button">Calculate</Button>
-							<p>{props.projects.data[0].description}</p>
-							{/* games[0] == undefined ? "" : games[0].Player.name */}
-							{/* {console.log(props)} */}
-								<img
-									id="image"
-									src={wood}
-									alt="Wood"
-									height="350"
-									width="400"
-									className="d flex"
-								></img>
-							</form>
-							<div className="form-check">
-									<InputGroup className="mb-3">
-								<label>
-										<InputGroup.Prepend>
-											<InputGroup.Checkbox aria-label="Checkbox for following text input" />
-										</InputGroup.Prepend>
-									Calculated materials.
-								</label>
-									</InputGroup>
+						</div>
+						<img
+							src={expl}
+							className="explodedDiagram"
+							alt="exploded-diagram"
+						/>
+						<img
+							id="image"
+							src={twentyfive}
+							className="overlay"
+							alt="progress"
+						/>
+					</Form>
+					<Form className="form-group2">
+						{["checkbox"].map((type) => (
+							<div key={`inline-${type}`} className="mb-3">
+								<Form.Check
+									label="Calculated materials."
+									type={type}
+									id={`inline-${type}-1`}
+								/>
+								<Form.Check
+									label="Purchased materials and prepared tools."
+									type={type}
+									id={`inline-${type}-2`}
+								/>
+								<Form.Check
+									label="Measured and cut all pieces and starting assembly. (disabled)"
+									type={type}
+									id={`inline-${type}-3`}
+								/>
+								<Form.Check
+									disabled
+									label="Completed build. (disabled)"
+									type={type}
+									id={`inline-${type}-3`}
+								/>
 							</div>
-
-							<div className="form-check">
-									<InputGroup className="mb-3">
-								<label>
-										<InputGroup.Prepend>
-											<InputGroup.Checkbox aria-label="Checkbox for following text input" />
-										</InputGroup.Prepend>
-									Purchased materials and prepared tools.
-								</label>
-									</InputGroup>
-							</div>
-
-							<div className="form-check">
-									<InputGroup className="mb-3">
-								<label>
-										<InputGroup.Prepend>
-											<InputGroup.Checkbox aria-label="Checkbox for following text input" />
-										</InputGroup.Prepend>
-									Measured and cut all pieces and starting assembly.
-								</label>
-									</InputGroup>
-							</div>
-
-							<div className="form-check">
-									<InputGroup className="mb-3">
-								<label>
-										<InputGroup.Prepend>
-											<InputGroup.Checkbox aria-label="Checkbox for following text input" />
-										</InputGroup.Prepend>
-									Completed build.
-								</label>
-									</InputGroup>
-							</div>
-						</section>
-						<button className="btn btn-primary mt-2" type="submit">
-							Save
+						))}
+						<button className="btn btn-secondary mt-2" type="submit">
+							Save Progress
 						</button>
+					</Form>
+					{/* </section> */}
 					</Row>
 				</Modal.Body>
-
 				{/* <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
