0780861d5aa7706b147b7a2f305cffc3e116b4ce
Luis Feliz <Luisfeliz@gmail.com>
2024-02-22 22:35:39 -0500

    clean code updating modal

--- client/src/components/ProjectButton/index.js
+++ client/src/components/ProjectButton/index.js
@@ -1,37 +1,31 @@
 import React, { useState, useEffect } from "react";
-// import { Grid, Segment } from "semantic-ui-react";
-import { Button, Modal, Container, Row, Col } from "react-bootstrap";
-// import Wrapper from "../components/Wrapper";
 
-import chair from "../../static/construction-icons/chair-main.svg";
-import bookcase from "../../static/construction-icons/bookcase-main.svg";
-import table from "../../static/construction-icons/table-main.svg";
-import desk from "../../static/construction-icons/desk-main.svg";
-import dresser from "../../static/construction-icons/dresser-main.svg";
-import tile from "../../static/construction-icons/tile-main.svg";
-import wall from "../../static/construction-icons/wall-main.svg";
-import ladder from "../../static/construction-icons/ladder-main.svg";
+import { Button, Modal, Container, Row, Col } from "react-bootstrap";
+
 import "./styles.css";
 // import "../ProjectButton/style.css";
-import Popup from "../Popup/index";
+import Popup from "../Popup/index.js";
 import wood from "../../static/wood.png";
 import API from "../../utils/API";
 
-function ProjectButton(props) {
+function ProjectButton({project}) {
+
+	const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    API.getAllProjects()
+      .then((res) => setProjects(res))
+      .catch((err) => console.log(err));
+  }, []);
+
+
+	
+
+
 	const [show, setShow] = useState(false);
-	const [projects, setProjects] = useState({});
-	const [index, setIndex] = useState(props.index);
 	const [pricing, setPricing] = useState([]);
 
-	useEffect(() => {
-		setShow(false);
-		API.getAllProjects()
-			.then((res) => setProjects(res))
-			.catch((err) => console.log(err));
-		API.getPricing()
-			.then((res) => setPricing(res))
-			.catch((err) => console.log(err));
-	}, []);
+	
 
 	return (
 		<Col>
@@ -42,26 +36,37 @@ function ProjectButton(props) {
 				onClick={() => setShow(true)}
 			>
 				<img
-					src={props.projects.data[props.index].imgMain}
+					src={project.imgMain}
 					className="icon"
-					id={(props.projects.data[props.index].projectName == "chair") ? "chair-change"
-						: (props.projects.data[props.index].projectName == "dresser") ? "dresser-change"
-						: (props.projects.data[props.index].projectName == "bookcase") ? "bookcase-change"
-						: (props.projects.data[props.index].projectName == "table") ? "table-change"
-						: (props.projects.data[props.index].projectName == "wall") ? "wall-change"
-						: (props.projects.data[props.index].projectName == "tile") ? "tile-change"
-						: (props.projects.data[props.index].projectName == "ladder") ? "ladder-change"
-						: (props.projects.data[props.index].projectName == "desk") ? "desk-change"
-						: null}
 					alt="furniture-icon"
+					onMouseOver={e => (e.currentTarget.src = project.imgEx)}
+					onMouseOut={e => (e.currentTarget.src = project.imgMain)}
 				/>
-				<p className="title-text">{props.projects.data[props.index].projectName}</p>
+				<p className="title-text">{project.projectName}</p>
+
+
 			</Button>
-			{projects.data && pricing.data ? (
-				<Popup projects={projects} pricing={pricing} show={show} index={index}
-				// dims={dims}
+			{projects.data
+          ? projects.data.map((result, index) => (
+              <div key={index} data-id={index} className="col-xs-6 col-md-4">
+                <Popup project={result} show={show} onHide={() => setShow(false)} />
+              </div>
+            ))
+          : null}
+			
+			{<Popup project={project} show={show} onHide={() => setShow(false)} />}
+
+
+			{/* {project && project ? (
+				<Popup project={project} pricing={pricing} show={false}
 				/>
-			) : null}
+			) : null} */}
+
+		
+
+			
+
+
 		</Col>
 	);
 }
--- client/src/pages/NewProject.js
+++ client/src/pages/NewProject.js
@@ -1,34 +1,33 @@
 import React, { useState, useEffect } from "react";
-// import { Grid, Segment } from "semantic-ui-react";
-import { Button, Modal, Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import ProjectButton from "../components/ProjectButton/index";
 import "./style.css";
-import Popup from "../components/Popup/index";
 import API from "../utils/API";
 
 function NewProject() {
-	const [project, setProject] = useState([]);
+  const [project, setProject] = useState([]);
 
-	useEffect(() => {
-		API.getAllProjects()
-			.then((res) => setProject(res))
-			.catch((err) => console.log(err));
-	}, []);
-	return (
-		<Container fluid className="wrap col-10">
-			<Row className="iconRow">
-				{project.data
-					? project.data.map((result, index) => (
-							<div key={index} data-id={index} className="col-xs-6 col-md-4">
-								<ProjectButton projects={project} index={index} />
-							</div>
-					))					
-					: null}
-			</Row>
-			<h6 className="create-title">Create a New Project</h6>
-		</Container>
-	);
-	console.log(project)
+  useEffect(() => {
+    API.getAllProjects()
+      .then((res) => setProject(res))
+      .catch((err) => console.log(err));
+  }, []);
+
+  
+  return (
+    <Container fluid className="wrap col-10">
+      <Row className="iconRow">
+        {project.data
+          ? project.data.map((result, index) => (
+              <div key={index} data-id={index} className="col-xs-6 col-md-4">
+                <ProjectButton project={result} />
+              </div>
+            ))
+          : null}
+      </Row>
+      <h6 className="create-title">Create a New Project</h6>
+    </Container>         
+  );
 }
 
 export default NewProject;
